b876046071b51b883426e13956739893035eb7a8
RyanAnderson <Ryan.Anderson@2itesting.com>
2022-09-27 16:08:08 +0100

    Changed how data is generated after running mvn test to allow use with PicklesUI

--- src/test/java/uk/co/testing/RunCucumberTest.java
+++ src/test/java/uk/co/testing/RunCucumberTest.java
@@ -10,6 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("uk/co/testing")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
